0f71e6f9b9b8bd9942fbfb89be923c0cc078efbf
modupimaphoto <modupimaphoto@gmail.com>
2023-09-08 09:26:52 -0700

    removed s on some word of byte

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@ int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
 	printf("Size of a int: %ld byte(s)\n", sizeof(int));
-	printf("size of a long int: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %ld bytes(s)", sizeof(float));
+	printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
 	return (0);
 }
