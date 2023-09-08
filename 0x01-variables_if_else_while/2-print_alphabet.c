1b900219bbf1c5d441bcb427acf518d2a3b782ce
modupimaphoto <modupimaphoto@gmail.com>
2023-09-08 11:40:13 -0700

    a program that prints the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry ponit
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
