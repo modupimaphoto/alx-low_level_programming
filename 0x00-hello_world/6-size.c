c13900c5e53807d5abf54edc9f3578dccc4b721f
modupimaphoto <modupimaphoto@gmail.com>
2023-09-08 09:19:44 -0700

    added betwee n the size of a dataype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %ld byte(s)\n", sizeof(char));
-	printf("Size of int: %ld byte(s)\n", sizeof(int));
-	printf("size of long int: %ld bytes(s)\n", sizeof(long int));
-	printf("Size of long long int: %ld bytes(s)\n", sizeof(long long int));
-	printf("Size of float: %ld bytes(s)", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of a int: %ld byte(s)\n", sizeof(int));
+	printf("size of a long int: %ld bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)", sizeof(float));
 	return (0);
 }
