26e03a3a0e07ba7b933d8ec49478f88ec937dcb4
modupimaphoto <modupimaphoto@gmail.com>
2023-09-12 08:06:52 -0700

    fixed for loop initial declarations are only allowed in C99 or C11 mode

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,16 +6,18 @@
  */
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (int minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
-			putchar(hour / 10 + '0');
-			putchar(hour % 10 + '0');
-			putchar(':');
-			putchar(minute / 10 + '0');
-			putchar(minute % 10 + '0');
-			putchar('\n');
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minute / 10 + '0');
+			_putchar(minute % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
