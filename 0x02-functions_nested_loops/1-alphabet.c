50c878f8bff2225b8fe6499a7ba2a759990a7d48
modupimaphoto <modupimaphoto@gmail.com>
2023-09-12 03:43:59 -0700

    fixed compilation errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,18 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void)
+{
+        char c = 'a';
+
+        while (c <= 'z')
+        {
+                _putchar(c);
+                ++c;
+        }
+        _putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
