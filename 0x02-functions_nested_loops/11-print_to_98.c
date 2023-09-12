e65f3e856306ec86757bc1534ae551f5b61f6c74
modupimaphoto <modupimaphoto@gmail.com>
2023-09-12 10:38:09 -0700

    typos errors

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,20 +11,18 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		printf("%d, ", n);
 		while (n >= 98)
 		{
-			printf("%d, ", i);
-			--i;
+			printf("%d, ", n);
+			--n;
 		}
 	}
 	else
 	{
-		printf("%d, ", n);
 		while (n <= 98)
 		{
-			printf("%d, ", i);
-			++i;
+			printf("%d, ", n);
+			++n;
 		}
 	}
 }
