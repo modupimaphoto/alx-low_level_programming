2c75783567bb675642e8090358f1f5481f0a2031
modupimaphoto <modupimaphoto@gmail.com>
2023-09-13 02:45:36 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,32 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n <= 98)
 	{
-		while (n >= 98)
+		for (; n <= 98; n++)
 		{
-			putchar(n);
-			putchar(',');
-			putchar(' ');
-			--n;
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
 		}
-		putchar('\n');
 	}
 	else
 	{
-		while (n <= 98)
+		for (; n >= 98; n--)
 		{
-			putchar(n);
-			putchar(',');
-			putchar(' ');
-			++n;
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
 		}
-		putchar('\n');
 	}
 }
