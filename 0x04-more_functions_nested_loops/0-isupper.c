a0e52cce44883c3629b8a250b541b1643ce57a02
modupimaphoto <modupimaphoto@gmail.com>
2023-09-14 03:14:16 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype>
+
+/**
+ * _isupper - checks for uppercase character.
+ *
+ * @c: character to check.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	return (0);
+}
