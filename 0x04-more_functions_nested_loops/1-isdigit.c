b6d033e8be9aefec22f228907e8a6f35ace81500
modupimaphoto <modupimaphoto@gmail.com>
2023-09-14 03:40:59 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9).
+ *
+ * @c: git to check.
+ *
+ * Return: Alwayss 0.
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
