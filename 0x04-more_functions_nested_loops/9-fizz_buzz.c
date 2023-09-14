002ec923cb3a1cbbe5bec18c1741592da82305d9
modupimaphoto <modupimaphoto@gmail.com>
2023-09-14 06:20:12 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the numbers from 1 to 100, followed by a new line,
+ * multiples of three print Fizz,
+ * for the multiples of five print Buzz,
+ * multiples of both three and five print FizzBuzz.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
