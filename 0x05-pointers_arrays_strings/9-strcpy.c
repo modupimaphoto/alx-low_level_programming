69a473d4b59f92891ab0dcbe1890d8a0205d4942
modupimaphoto <modupimaphoto@gmail.com>
2023-09-19 01:14:16 -0700

    9. strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed by the src.
+ * @dest: char type string.
+ * @src: char type string.
+ * Return: pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int z = -1;
+
+	do {
+		z++;
+		dest[z] = src[z];
+	} while (src[z] != '\0');
+	return (dest);
+}
