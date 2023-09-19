908285143c414b91fb3fea9718af128b1be79b5a
modupimaphoto <modupimaphoto@gmail.com>
2023-09-18 23:17:19 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - eturns the length of a string.
+ *
+ * @s: string to check.
+ *
+ * Return: Always string length.
+ */
+int _strlen(char *s)
+{
+	int str_len = strlen(s);
+
+	return (str_len);
+}
