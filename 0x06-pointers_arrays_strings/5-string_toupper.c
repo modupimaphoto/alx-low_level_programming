5b32e4e0014c7991e752479752674893b2586141
modupimaphoto <modupimaphoto@gmail.com>
2023-09-20 11:41:06 -0700

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,10 +2,21 @@
 
 /**
  * string_toupper -  changes all lowercase letters of a string to uppercase.
- * @' ': string type.
+ * @x: string type.
  * Return: string type.
  */
-char *string_toupper(char *)
+char *string_toupper(char *x)
 {
+	int len;
 
+	len = 0;
+	while (x[len] != '\0')
+	{
+		if (x[len] >= 97 && x[len] <= 122)
+		{
+			x[len] = x[len] - 32;
+		}
+		len++;
+	}
+	return (x);
 }
