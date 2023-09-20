c738bfe4ece93ae439f73fdca9945aca3ff0db35
modupimaphoto <modupimaphoto@gmail.com>
2023-09-20 11:55:26 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,10 +2,32 @@
 
 /**
  * cap_string - capitalizes all words of a string.
- * @*'': string type.
+ * @s: string type.
  * Return: string type.
  */
-char *cap_string(char *)
+char *cap_string(char *s)
 {
+	int string_count;
 
+	string_count = 0;
+	while (s[string_count] != '\0')
+	{
+		if (s[0] >= 97 && s[0] <= 122)
+		{
+			s[0] = s[0] - 32;
+		}
+		if (s[string_count] == ' ' || s[string_count] == '\t'
+		|| s[string_count] == '\n'
+		|| s[string_count] == ',' || s[string_count] == ';' || s[string_count] == '.'
+		|| s[string_count] == '"' || s[string_count] == '!' || s[string_count] == '?'
+		|| s[string_count] == '{' || s[string_count] == '}')
+		{
+			if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+			{
+				s[string_count + 1] = s[string_count + 1] - 32;
+			}
+		}
+		string_count++;
+	}
+	return (s);
 }
