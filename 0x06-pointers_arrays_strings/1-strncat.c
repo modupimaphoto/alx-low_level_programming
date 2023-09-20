af36a8504f492f02609fd9dde2fe3083f360ccad
modupimaphoto <modupimaphoto@gmail.com>
2023-09-20 11:07:19 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strncat - concatenates two strings.
  * @dest: type string.
@@ -9,5 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
+	int len, z;
 
+	len = 0;
+	while (dest[len] != '\0')
+		len++;
+	for (z = 0; z < n && src[z] != '\0'; z++, len++)
+		dest[len] = src[z];
+	dest[len] = '\0';
+	return (dest);
 }
