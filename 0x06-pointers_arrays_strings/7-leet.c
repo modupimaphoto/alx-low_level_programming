c080f7457b5adea4ac50d948ae1f56622ebf1051
modupimaphoto <modupimaphoto@gmail.com>
2023-09-20 12:03:57 -0700

    7. Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,10 +2,28 @@
 
 /**
  * leet -  encodes a string into 1337.
- * @'': string type.
+ * @s: string type.
  * Return: string type.
  */
-char *leet(char *)
+char *leet(char *s)
 {
+	int len, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
 
+	len = 0;
+	while (s[len] != '\0')
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[len])
+			{
+				s[len] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		len++;
+	}
+	return (s);
 }
