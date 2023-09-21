d46763c74f30f54648625e50edc30d6fb9f25f08
modupimaphoto <modupimaphoto@gmail.com>
2023-09-21 00:04:09 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: string to encode.
+ * Return: Always pointer.
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if ((*ptr >= 'A' && *ptr <= 'M') || (*ptr >= 'a' && *ptr <= 'm'))
+		{
+			*ptr += 13;
+		}
+		else if ((*ptr >= 'N' && *ptr <= 'Z') || (*ptr >= 'n' && *ptr <= 'z'))
+		{
+			*ptr -= 13;
+		}
+		ptr++;
+	}
+	return (str);
+}
