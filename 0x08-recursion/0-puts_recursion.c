cf23789d5535cea9ebe71a4459b2f79ecf3ed035
modupimaphoto <modupimaphoto@gmail.com>
2023-09-27 02:22:29 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _puts_recursion -  prints a string, followed by a new line.
  * @s: string to print.
  */
 void _puts_recursion(char *s)
 {
-
+	puts(s);
+	_putchar('\n');
 }
