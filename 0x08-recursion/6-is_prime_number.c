d2ea3352a19eb9db16819d596fa263b070f3f668
modupimaphoto <modupimaphoto@gmail.com>
2023-09-27 07:04:45 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
+ * is_prime_recursive_helper - divides the number n
+ * by increasing divisors starting from 2.
+ * @n: fisrt parameter.
+ * @divisor: second parameter.
+ * Return: returns 0 (not a prime number).
+ */
+int is_prime_recursive_helper(int n, int divisor)
+{
+	if (divisor == n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_recursive_helper(n, divisor + 1));
+}
+/**
  * is_prime_number - check is number is prine number.
  * @n: number to check.
  * Return: 1 or 0.
  */
 int is_prime_number(int n)
 {
-
+	if (n <= 1)
+		return (0);
+	return (is_prime_recursive_helper(n, 2));
 }
