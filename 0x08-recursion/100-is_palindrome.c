9a9116704cc8606afd3eb63f4d992ca4305ba3e5
modupimaphoto <modupimaphoto@gmail.com>
2023-09-27 01:48:23 -0700

    advanced project

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ * @s: string to check.
+ * Return:  1 or 0.
+ */
+int is_palindrome(char *s)
+{
+
+}
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings.
+ * @s1: first paramter.
+ * @s2: second paramter.
+ * Return: 1 or 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
