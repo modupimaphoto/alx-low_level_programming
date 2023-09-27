fab4c387cdb4e2de79034957be1d3a9a96f40d25
modupimaphoto <modupimaphoto@gmail.com>
2023-09-27 06:37:31 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,34 @@
 #include "main.h"
 
 /**
+ * sqrt_recursive_helper - recursion helper.
+ * @n: firtst parameter.
+ * @start: second paramter.
+ * @end: third parameter.
+ * Return: recursive calculation.
+ */
+int sqrt_recursive_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (mid * mid == n)
+		return (mid);
+	if (start >= end)
+		return (-1);
+	if (mid * mid > n)
+		return (sqrt_recursive_helper(n, start, mid - 1));
+	return (sqrt_recursive_helper(n, mid + 1, end));
+}
+/**
  * _sqrt_recursion -  returns the natural square root of a number.
  * @n: parameter.
  * Return: results.
  */
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_recursive_helper(n, 1, n));
 }
