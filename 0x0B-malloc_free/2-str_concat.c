4dc9ac1b8866e81e248602a19600708e7a4150e0
modupimaphoto <modupimaphoto@gmail.com>
2023-10-02 04:02:25 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <string.h>
+#include <stdlib.h>
 /**
  * str_concat - concatenates two strings.
  * @s1: first string parameter.
@@ -7,5 +9,23 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	size_t len1, len2;
+	char *result;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	result = malloc((len1 + len2 + 1) * sizeof(char));
+
+	if (result == NULL)
+		return (NULL);
+
+	strcpy(result, s1);
+	strcat(result, s2);
+	return (result);
 }
