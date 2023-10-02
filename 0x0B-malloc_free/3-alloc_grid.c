03ccacc993483b1cb55f429744d28a36e824165d
modupimaphoto <modupimaphoto@gmail.com>
2023-10-02 04:16:49 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * alloc_grid - returns a pointer to a 2 dimensional array of integers.
  * @width: width of the dimesional.
@@ -7,5 +8,28 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int  **grid;
+	int a, b, c;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		grid[a] = malloc(width * sizeof(int));
+		if (grid[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+				free(grid[b]);
+			free(grid);
+			return (NULL);
+		}
+		for (c = 0; c < width; c++)
+			grid[a][c] = 0;
+	}
+	return (grid);
 }
