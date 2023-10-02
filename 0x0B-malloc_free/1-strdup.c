b41a99a076e51140796077c9f52961d429c8eeed
modupimaphoto <modupimaphoto@gmail.com>
2023-10-02 03:37:13 -0700

    check 11 correct input

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,9 +8,20 @@
  */
 char *_strdup(char *str)
 {
-	char *copy = strdup(str);
+	char *new_str;
+	unsigned int i, len;
 
-	if (str == NULL || copy == NULL)
+	if (str == NULL)
 		return (NULL);
-	return (copy);
+
+	len = strlen(str);
+	new_str = malloc(sizeof(char) * (len + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		new_str[i] = str[i];
+
+	new_str[i] = '\0';
+	return (new_str);
 }
