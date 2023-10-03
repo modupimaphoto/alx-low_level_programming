808556a31a7982d7395fee0a5078d46dd243a41f
modupimaphoto <modupimaphoto@gmail.com>
2023-10-03 01:04:29 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * argstostr - concatenates all the arguments of your program.
  * @ac: int type.
@@ -7,5 +9,29 @@
  */
 char *argstostr(int ac, char **av)
 {
+	int i, j, len = 0, k = 0;
+	char *str;
 
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+		len += strlen(av[i]) + 1;
+
+	str = malloc(sizeof(char) * len);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+	str[k] = '\0';
+	return (str);
 }
