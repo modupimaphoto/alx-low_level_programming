d660428a6dc4f97ee45dde5eeb2647bdc12879af
modupimaphoto <modupimaphoto@gmail.com>
2023-10-03 00:53:48 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 /**
  * free_grid -  frees a 2 dimensional grid previously.
  * @grid: int type.
@@ -6,5 +7,9 @@
  */
 void free_grid(int **grid, int height)
 {
+	int i;
 
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
 }
