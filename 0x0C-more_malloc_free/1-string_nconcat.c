08ab0d4183e4cbeccde27386dbf5f118cbd18904
modupimaphoto <modupimaphoto@gmail.com>
2023-10-05 03:24:46 -0700

    correct output

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,29 +11,26 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int len1 = 0, len2 = 0;
-	unsigned int i;
+	unsigned int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
 	if (n >= len2)
 		n = len2;
-	ptr = malloc(sizeof(char) * (len1 + n + 1));
+
+	ptr = (char *) malloc((len1 + n + 1) * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
-	for (i = 0; i < len2; i++)
-		ptr[len1 + i] = s2[i];
-	ptr[len1 + n] = '\0';
+	strcpy(ptr, s1);
+
+	strncat(ptr, s2, n);
 
 	return (ptr);
 }
