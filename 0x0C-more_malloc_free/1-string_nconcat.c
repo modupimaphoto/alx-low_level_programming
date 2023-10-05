3ee0e16c524dbfbef9aa57bdf8508cc96fbd6586
modupimaphoto <modupimaphoto@gmail.com>
2023-10-05 03:12:23 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * string_nconcat -  concatenates two strings.
  * @s1: first parameter.
@@ -9,5 +10,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *ptr;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int i;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
+	if (n >= len2)
+		n = len2;
+	ptr = malloc(sizeof(char) * (len1 + n + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+	for (i = 0; i < len2; i++)
+		ptr[len1 + i] = s2[i];
+	ptr[len1 + n] = '\0';
+
+	return (ptr);
 }
