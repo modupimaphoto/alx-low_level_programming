203ddb56d9d643702c8365cffed66e15f3b11568
modupimaphoto <modupimaphoto@gmail.com>
2023-10-05 03:33:13 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc -  allocates memory for an array, using malloc.
  * @nmemb: an array.
@@ -8,5 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int totalSize;
+	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	totalSize = nmemb * size;
+	ptr = malloc(totalSize);
+	if (ptr == NULL)
+		return (NULL);
+	memset(ptr, 0, totalSize);
+
+	return (ptr);
 }
