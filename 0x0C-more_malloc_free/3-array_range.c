742a7d8f7473a9eb8176707087c527f73c59d22a
modupimaphoto <modupimaphoto@gmail.com>
2023-10-05 03:44:09 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,5 +8,23 @@
  */
 int *array_range(int min, int max)
 {
+	int size;
+	int *arr;
+	int i;
 
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	arr = (int *)malloc(size * sizeof(int));
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = min;
+		min++;
+	}
+	return (arr);
 }
