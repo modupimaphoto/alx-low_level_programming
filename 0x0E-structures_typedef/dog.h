1931013ba013aac2ce3414c34b58f25bfe41d4fb
modupimaphoto <modupimaphoto@gmail.com>
2023-10-09 02:09:58 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER_FILE_NAME_H
+#define HEADER_FILE_NAME_H
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* HEADER_FILE_NAME_H */
