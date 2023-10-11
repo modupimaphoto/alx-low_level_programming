1e1460d7824cf3a7bb177ef2441e33b9864addc6
modupimaphoto <modupimaphoto@gmail.com>
2023-10-11 06:06:46 -0700

    validating check 13 and 17

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,12 +20,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (s[0] == ops[i].op[0])
-		{
-			return (ops[i].f);
-		}
+		return (ops[i].f);
 		i++;
 	}
 	return (NULL);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,25 +9,31 @@
  *
  * Return: 0 on success, 98 on failure
  */
-int main(int argc, char **argv)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int num1, num2, result;
-	int (*op_func)(int, int);
+	int num1, num2;
+	char *op;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		return (98);
+		exit(98);
 	}
+
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL || argv[2][1] != '\0')
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		return (99);
+		exit(99);
 	}
-	result = op_func(num1, num2);
-	printf("%d\n", result);
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		 exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
