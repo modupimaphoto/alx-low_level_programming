e855f7ff8d58394853c078098ec2d43102ff89e5
modupimaphoto <modupimaphoto@gmail.com>
2023-10-11 06:35:09 -0700

    remove ‘strcmp’ function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,11 +20,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
