0f2a7321c9fdd2aca2554257fcc5e87c77100c22
modupimaphoto <modupimaphoto@gmail.com>
2023-10-12 02:57:39 -0700

    Remove validating if separator is null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
