8a9057b660ffaa66fdb8cc5aeac38887c67fae05
modupimaphoto <modupimaphoto@gmail.com>
2023-10-19 05:17:40 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list -  frees a list_t list.
+ * @head: first node
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
